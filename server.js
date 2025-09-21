cdfbf59caa1ec158bd1eeeea225a9d6ac8b8024e
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-09-21 19:18:45 +0200

    error fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const contactsRouter = require('./routes/contacts');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

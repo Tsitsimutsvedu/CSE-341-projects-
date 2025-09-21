8da31f22e0c3315938839718e3212badf8e92909
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-09-21 12:31:39 +0200

    error

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+
 const mongoose = require('mongoose');
+
 const contactsRouter = require('./routes/contacts');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use('/contacts', contactsRouter);
